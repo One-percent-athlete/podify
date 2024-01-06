c20c37763c5703544673ad9b4b8b7f43f05b72cc
One-percent-athlete <one.percent.athlete@gmail.com>
2024-01-06 20:54:09 +0900

    Add app notification function

--- Mypod/src/views/Upload.tsx
+++ Mypod/src/views/Upload.tsx
@@ -1,8 +1,11 @@
 import CategorySelector from '@components/CategorySelector';
 import FileSelector from '@components/FileSelector';
 import AppButton from '@ui/AppButton';
+import Progress from '@ui/Progress';
+import {Keys, getFromAsyncStorage} from '@utils/asyncStorage';
 import {categories} from '@utils/categorise';
 import colors from '@utils/colors';
+import {mapRange} from '@utils/math';
 import {FC, useState} from 'react';
 import {
   Pressable,
@@ -12,15 +15,107 @@ import {
   TextInput,
   View,
 } from 'react-native';
+import {DocumentPickerResponse, types} from 'react-native-document-picker';
 import Material from 'react-native-vector-icons/MaterialCommunityIcons';
+import client from 'src/api/client';
+import * as yup from 'yup';
+
+interface FormFields {
+  title: string;
+  category: string;
+  about: string;
+  file?: DocumentPickerResponse;
+  poster?: DocumentPickerResponse;
+}
+
+const defaultForm: FormFields = {
+  title: '',
+  category: '',
+  about: '',
+  file: undefined,
+  poster: undefined,
+};
+
+const audioInfoSchema = yup.object().shape({
+  title: yup.string().trim().required('Title is missing.'),
+  category: yup.string().oneOf(categories, 'Category is missing.'),
+  about: yup.string().trim().required('About is missing.'),
+  file: yup.object().shape({
+    uri: yup.string().required('Audio file is missing.'),
+    name: yup.string().required('Audio file is missing.'),
+    type: yup.string().required('Audio file is missing.'),
+    size: yup.number().required('Audio file is missing.'),
+  }),
+  poster: yup.object().shape({
+    uri: yup.string(),
+    name: yup.string(),
+    type: yup.string(),
+    size: yup.number(),
+  }),
+});
+
 interface Props {}
 
 const Upload: FC<Props> = props => {
   const [showCategoryModal, setShowCategoryModal] = useState(false);
+  const [audioInfo, setAudioInfo] = useState({...defaultForm});
+  const [uplodaProgress, setUploadProgress] = useState(0);
+  const [busy, setBusy] = useState(false);
 
-  const [audioInfo, setAudioInfo] = useState({
-    category: '',
-  });
+  const handleUpload = async () => {
+    setBusy(true);
+    try {
+      const finalData = await audioInfoSchema.validate(audioInfo);
+
+      const formData = new FormData();
+
+      formData.append('title', finalData.title);
+      formData.append('about', finalData.about);
+      formData.append('category', finalData.category);
+      formData.append('file', {
+        name: finalData.file.name,
+        type: finalData.file.type,
+        uri: finalData.file.uri,
+      });
+
+      if (finalData.poster.uri) {
+        formData.append('poster', {
+          name: finalData.poster.name,
+          type: finalData.poster.type,
+          uri: finalData.poster.uri,
+        });
+      }
+      const token = await getFromAsyncStorage(Keys.AUTH_TOKEN);
+      const {data} = await client.post('/audio/create', formData, {
+        headers: {
+          Authorization: 'Bearer ' + token,
+          'Content-Type': 'multipart/form-data;',
+        },
+        onUploadProgress(progressEvent) {
+          const uploaded = mapRange({
+            inputMin: 0,
+            inputMax: progressEvent.total || 0,
+            outputMin: 0,
+            outputMax: 100,
+            inputValue: progressEvent.loaded,
+          });
+
+          if (uploaded >= 100) {
+            setAudioInfo({...defaultForm});
+            setBusy(false);
+          }
+
+          setUploadProgress(Math.floor(uploaded));
+        },
+      });
+      console.log(data);
+    } catch (error) {
+      if (error instanceof yup.ValidationError)
+        console.log('Validation error: ', error.message);
+      else console.log(error.response.data);
+    }
+    setBusy(false);
+  };
 
   return (
     <ScrollView style={styles.container}>
@@ -29,8 +124,12 @@ const Upload: FC<Props> = props => {
           icon={
             <Material name="image-outline" size={35} color={colors.SECONDARY} />
           }
-          btnTitle="Select Cover"
+          btnTitle="Select Poster"
           style={{marginLeft: 20}}
+          options={{type: [types.images]}}
+          onSelect={poster => {
+            setAudioInfo({...audioInfo, poster});
+          }}
         />
         <FileSelector
           icon={
@@ -42,6 +141,10 @@ const Upload: FC<Props> = props => {
           }
           btnTitle="Select Audio"
           style={{marginLeft: 20}}
+          options={{type: [types.audio]}}
+          onSelect={file => {
+            setAudioInfo({...audioInfo, file});
+          }}
         />
       </View>
       <View style={styles.formContainer}>
@@ -49,6 +152,10 @@ const Upload: FC<Props> = props => {
           placeholderTextColor={colors.INACTIVE_CONTRAST}
           placeholder="Title"
           style={styles.input}
+          onChangeText={text => {
+            setAudioInfo({...audioInfo, title: text});
+          }}
+          value={audioInfo.title}
         />
         <Pressable
           onPress={() => {
@@ -65,6 +172,10 @@ const Upload: FC<Props> = props => {
           style={styles.input}
           numberOfLines={5}
           multiline
+          onChangeText={text => {
+            setAudioInfo({...audioInfo, about: text});
+          }}
+          value={audioInfo.about}
         />
 
         <CategorySelector
@@ -78,13 +189,20 @@ const Upload: FC<Props> = props => {
             return <Text style={styles.category}>{item}</Text>;
           }}
           onSelect={item => {
-            setAudioInfo({category: item});
+            setAudioInfo({...audioInfo, category: item});
           }}
         />
 
-        <View style={{marginBottom: 20}} />
+        <View style={{marginVertical: 20}}>
+          {busy ? <Progress progress={uplodaProgress} /> : null}
+        </View>
 
-        <AppButton borderRadius={7} title="Submit" />
+        <AppButton
+          busy={busy}
+          borderRadius={7}
+          title="Submit"
+          onPress={handleUpload}
+        />
       </View>
     </ScrollView>
   );
